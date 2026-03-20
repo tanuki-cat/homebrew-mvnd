b1a4211c4436946a9a2378727fbdf7f6fc67fdd6
Guillaume Nodet <gnodet@gmail.com>
2026-03-20 13:09:08 +0100

    Release 1.0.5

--- Formula/mvnd.rb
+++ Formula/mvnd.rb
@@ -2,15 +2,15 @@ class Mvnd < Formula
   desc "Apache Maven Daemon"
   homepage "https://github.com/apache/maven-mvnd"
   license "Apache-2.0"
-  version "2.0.0-rc-3"
+  version "1.0.5"
   on_macos do
     on_intel do
-      url "https://downloads.apache.org/maven/mvnd/2.0.0-rc-3/maven-mvnd-2.0.0-rc-3-darwin-amd64.zip"
-      sha256 "59cac90cf2083e418d8b0e7296b5a48e5e29589c25c3bb94b76cb8da18b43bc7"
+      url "https://downloads.apache.org/maven/mvnd/1.0.5/maven-mvnd-1.0.5-darwin-amd64.zip"
+      sha256 "95e12908f24fd018ee41e5d31fe43a86340877e9b937651732e310b250411de3"
     end
     on_arm do
-      url "https://downloads.apache.org/maven/mvnd/2.0.0-rc-3/maven-mvnd-2.0.0-rc-3-darwin-aarch64.zip"
-      sha256 "3dd88c7d70bb1b5ebfc8fc98fe39f97c23c3b3c5c5bf0faea49baf2f485c9705"
+      url "https://downloads.apache.org/maven/mvnd/1.0.5/maven-mvnd-1.0.5-darwin-aarch64.zip"
+      sha256 "bd98f847478de20158242f6cce6d9bc6cb6e3e9ba7b932237f0122f52a8de5a3"
     end
   end
   on_linux do
